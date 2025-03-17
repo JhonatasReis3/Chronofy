539c0df4b06e9f7207088a9a52db76e96fe058a2
JhonatasReis3 <programador.jhonatasreis@gmail.com>
2025-03-17 09:59:21 -0300

    Chronofy 0.0.0

---
+++ public/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chronofy</title>
+    <link rel="stylesheet" href="./css/tailwind.css">
+</head>
+
+<body class="bg-Backgrounds01">
+    <main class="flex w-full h-screen">
+        <div class="bg-Backgrounds02 w-2/5 h-full p-15 flex flex-col gap-5">
+            <div class="bg-Titulos w-50 h-10">LOGO</div>
+            <form action="" class="w-full h-full bg-Subtitulos flex flex-col gap-5">
+                <div class="w-5/10 h-10 bg-Titulos">
+                    texto bem vindo
+                </div>
+                <div class="flex flex-col gap-10">
+                    <div class=" flex gap-3">
+                        <input type="text" name="" id="" class="inputs">
+                        <input type="text" name="" id="" class="inputs">
+                    </div>
+                    <input type="email" name="" id="" class="inputs">
+                    <input type="password" name="" id="" class="inputs">
+                    <input type="password" name="" id="" class="inputs">
+                </div>
+                <div class="">
+                    <input type="submit" value="" class="w-full h-12.5 bg-DetalhesVerde">
+                </div>
+                <div class="flex justify-center items-center w-full h-3">
+                    <div class="w-2/10 h-full bg-DetalhesLaranja"></div>
+                </div>
+                <div class="flex gap-3">
+                    <div class="w-full h-15 bg-Titulos"></div>
+                    <div class="w-full h-15 bg-Titulos"></div>
+                </div>
+                <div class="flex justify-center items-center w-full h-3">
+                    <div class="w-3/10 h-full bg-DetalhesVerde"></div>
+                </div>
+            </form>
+        </div>
+        <div class="w-3/5 h-full p-15"></div>
+    </main>
+    <style>
+    .inputs {
+        width: 100%;
+        height: 50px;
+        padding: 8;
+        background-color: #0b0011;
+    }
+    </style>
+</body>
+
+</html>
